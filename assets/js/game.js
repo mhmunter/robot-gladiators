177e9aea7f623a1ad64c19aa982daa7e906b972c
M Munter <mark.munter1@gmail.com>
2022-02-15 09:54:54 -0600

    added money/skip fightfunction

--- assets/js/game.js
+++ assets/js/game.js
@@ -24,6 +24,7 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
@@ -71,4 +72,25 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     window.alert(playerName + " has chosen to skip the fight!");
   } else {
     window.alert("You need to choose a valid option. Try again!");
+  // if yes (true), leave fight
+  if (confirmSkip) {
+    window.alert(playerName + " has decided to skip this fight. Goodbye!");
+    // subtract money from playerMoney for skipping
+    playerMoney = playerMoney - 2;
   }
+  // if no (false), ask question again by running fight() again
+  else {
+    fight();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
