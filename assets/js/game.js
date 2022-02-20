f8b43e93175708f99e8d88c764e4d353781a8959
M Munter <mark.munter1@gmail.com>
2022-02-20 11:04:46 -0600

    Revert "added endne game. resart functions"
    This reverts commit aa762f9001d47fe4519a1535e0d6836c994b3a59.

--- assets/js/game.js
+++ assets/js/game.js
@@ -37,8 +37,7 @@ console.log(enemyNames[3]);
 
 // fight function (now with parameter for enemy's name)
 var fight = function(enemyName) {
-  // ||
-  while (playerHealth > 0 && enemyHealth > 0) {  
+  while (playerHealth > 0 && enemyHealth > 0) {
     // ask player if they'd like to fight or run
     var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
@@ -98,7 +97,6 @@ var fight = function(enemyName) {
 //   // if player is still alive, keep fighting
 //   // function to start a new game 
 // }
-                                      // +1
 var startGame = function() {
   for (var i = 0; i < enemyNames.length; i++) {
   if (playerHealth > 0) {
@@ -125,22 +123,7 @@ var startGame = function() {
     break;
   }
 }
+
+};
 // start the game when the page loads
 startGame();
-playerHealth = 100;
-  playerAttack = 10;
-  playerMoney = 10;
-};
-
-var endGame = function() {
-  // if player is still alive, player wins!
-  if (playerHealth > 0) {
-    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
-  } if (playAgainConfirm) {
-    // restart the game
-    startGame();
-  } 
-  else {
-    window.alert("You've lost your robot in battle.");
-  }
-}
